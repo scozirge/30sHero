c04bb310c488ea69776e320dbf1ce7d020682cb2
scozirge <scozirge2@gmail.com>
2018-10-12 15:11:29 +0800

    server

--- 30SecHero/Server/QuickSignUp.php
+++ 30SecHero/Server/QuickSignUp.php
@@ -5,74 +5,76 @@ require_once('./config.php');
 //導入加密類
 require_once('./3DES.php');
 
-//$requestTime = $_POST['RequestTime'];
 //取得建立帳戶時間，格式為年份/月/日 時:分:秒(台北時區)
 date_default_timezone_set('Asia/Taipei');
 $signUpTime= date("Y/m/d H:i:s");
-$ac=substr(md5($signUpTime),3,11);
+//接收資料
+$ac_K=$_POST['ac_K'];
+$userID_K=$_POST['userID_K'];
+//找尋Kongergate UserID
 $con_l = mysql_connect($db_host_load,$db_user,$db_pass) or ("Fail:1:"  . mysql_error());
 if (!$con_l)
 	die('Fail:1:' . mysql_error());
-mysql_select_db($db_name , $con_l) or die ("Fail:6:" . mysql_error());
-//$check = mysql_query("SELECT * FROM slotappdb.playeraccount WHERE `Account`='".$md5AC."'",$con_l);
-//$numrows = mysql_num_rows($check);
-$numrows=0;
-if ($numrows == 0)
-{
-	//使用本機資料
-	$score=$_POST['BestScore'];
-	$kills=$_POST['Kills'];
-	$shot=$_POST['Shot'];
-	$criticalHit=$_POST['CriticalHit'];
-	$death=$_POST['Death'];
-	$criticalCombo=$_POST['CriticalCombo'];		
-	$name="unnamed";
-	//使用Server資料
-    // $score=0;
-    // $kills=0;
-	// $shot=0;
-	// $criticalHit=0;
-	// $death=0;
-	// $criticalCombo=0;	
-	// $name="unnamed";
+mysql_select_db($db_name , $con_l) or die ("Fail:1:" . mysql_error());
+$check = mysql_query("SELECT * FROM ".$db_name.".playeraccount WHERE `userID_K`=".$userID,$con_l);
+$numrows = mysql_num_rows($check);
+//新增寫入DB連線
+$con_w = mysql_connect($db_host_write,$db_user,$db_pass,true) or ("Fail:1:"  . mysql_error());
 	
-    //$loginCode=substr(md5($lastLogin),-5);
-    //新增寫入DB連線
-    $con_w = mysql_connect($db_host_write,$db_user,$db_pass,true) or ("Fail:1:"  . mysql_error());
+if ($numrows == 0)//找不到已經存在的Kongregate帳號就創新帳號
+{
+
     if (!$con_w)
         die('Fail:1:' . mysql_error());
-    $signUpResult = mysql_query("INSERT INTO  ".$db_name.".playeraccount (   `account` ,`name` ,`score`,`kills`,`shot`,`criticalHit`,`death`,`criticalCombo`,`SignInTime`,`SignUpTime`) VALUES ( '".$ac."' , '".$name."', '".$score."' , '".$kills."' , '".$shot."' , '".$criticalHit."' , '".$death."' , '".$criticalCombo."','".$signUpTime."','".$signUpTime."') ; ",$con_w);
+	$gold=100;
+	$emerald=0;
+	//寫入資料庫
+    $signUpResult = mysql_query("INSERT INTO  ".$db_name.".playeraccount (  `userID_K` ,`gold` ,`emerald`,`signUpTime`,`signInTime`) VALUES ( '".$ac_K."','".$userID_K."','".$gold."','".$emerald."' ,'".$signUpTime."','".$signUpTime."') ; ",$con_w);
 	if ($signUpResult)
 	{
 		//寫入玩家名稱player+流水號
-		$name="player".mysql_insert_id();
-		$set = mysql_query("UPDATE ".$db_name.".playeraccount SET `name` = '".$name."' WHERE `account` = '".$ac."' ",$con_w);
-        if(!$set)
-		{
-			$time_end = microtime(true);
-			$executeTime = $time_end - $time_start;
-			die ("Fail:12: \nExecuteTime=".$executeTime."");
-		}
+		$id=mysql_insert_id();
        //對帳號進行加密
         $rep = new Crypt3Des (); // new一個加密類
         $ACPass=$rep->encrypt ( "u.6vu4".$ac."gk4ru4");
         //計算執行時間
         $time_end = microtime(true);
         $executeTime = $time_end - $time_start;
-		//使用server資料
-        //die("Success:".$ac.",".$ACPass.",".$name.",".$score.",".$kills.",".$shot.",".$criticalHit.",".$death.",".$criticalCombo.": \nExecuteTime=".$executeTime);
-		//使用本機資料 
-		die("Success:".$ac.",".$ACPass.",".$name.": \nExecuteTime=".$executeTime);
+		//送回client
+		die("Success:".$id.",".$gold.",".$emerald.": \nExecuteTime=".$executeTime);
 	}
 	else
 	{
 		$time_end = microtime(true);
 		$executeTime = $time_end - $time_start;
-		die ("Fail:12: \nExecuteTime=".$executeTime."");
+		die ("Fail:5: \nExecuteTime=".$executeTime."");
 	}
 }
-else
+else//找到Kongregate帳戶就進行登入
 {
-	die("Fail:9");
+	 if (!$con_w)
+		die('Fail:1:' . mysql_error());
+	$id=0;
+	while($row = mysql_fetch_assoc($check))
+	{
+		$id=$row['id'];
+	}
+	$set = mysql_query("UPDATE ".$db_name.".playeraccount SET `id` = '".$id."',`signInTime` = '".$signUpTime."' WHERE `userID_K` = '".$userID_K."' ",$con_w);
+	if ($set)
+	{
+		//計算執行時間
+		$time_end = microtime(true);
+		$executeTime = $time_end - $time_start;
+		//送回client
+		die("Success:".$id.",".$gold.",".$emerald.": \nExecuteTime=".$executeTime);
+	}
+	else
+	{
+		//計算執行時間
+		$time_end = microtime(true);
+		$executeTime = $time_end - $time_start;
+		die ("Fail:5: \nExecuteTime=".$executeTime."");
+	}
 }
+
 ?>
