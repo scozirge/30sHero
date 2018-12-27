2637076eae5772f95d3c4c344f504924437437ea
scozirge <scozirge2@gmail.com>
2018-12-27 16:05:25 +0800

    test

---
+++ 30SecHero/30SecHero/Assets/Scripts/Tools/ScenePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ScenePlayer : MonoBehaviour
+{
+    static string NextSceneName;
+    public static void SetNextScene_static(string _sceneName)
+    {
+        NextSceneName = _sceneName;
+    }
+    public void SetNextScene(string _sceneName)
+    {
+        NextSceneName = _sceneName;
+    }
+    public void GoToNextScnee()
+    {
+        if (NextSceneName == "")
+            return;
+        SceneManager.LoadScene(NextSceneName);
+        NextSceneName = "";
+    }
+}
